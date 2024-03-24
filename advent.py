9e078fc07d7baf85d0225d039d3cdb39b20de3b4
John <sozhran@gmail.com>
2024-03-24 21:59:48 +0100

    day 3.1

--- advent.py
+++ advent.py
@@ -2,18 +2,17 @@
 
 day1_read_file = open('./day1.txt', 'r')
 day1_content = day1_read_file.read().split('\n')
+day1_input = [int(i) for i in day1_content]
 
 day2_read_file = open('./day2.txt', 'r')
-day2_content = day2_read_file.read().split('\n')
+day2_input = day2_read_file.read().split('\n')
 
 day3_read_file = open('./day3.txt', 'r')
-day3_content = day2_read_file.read().split('\n')
+day3_input = day3_read_file.read().split('\n')
 
 # DAY 1
 
 # Challenge 1.1
-
-day1_input = [int(i) for i in day1_content]
 
 ch11_count = 0
 
@@ -21,7 +20,7 @@ for i in range(len(day1_content) -1):
    if int(day1_input[i] < day1_input[i+1]):
        ch11_count += 1
         
-# print("Challenge 1.1 answer:", ch11_count)
+print("Challenge 1.1 answer:", ch11_count)
 
 # Challenge 1.2
 
@@ -34,7 +33,7 @@ for i in range(0, len(day1_input) - 3):
     if day1_input[i+3] > day1_input[i]:
         ch12_count += 1
 
-# print("Challenge 1.2 answer:", ch12_count)
+print("Challenge 1.2 answer:", ch12_count)
 
 # DAY 2
 
@@ -43,7 +42,7 @@ for i in range(0, len(day1_input) - 3):
 ch21_count_horizontal = 0
 ch21_count_depth = 0
 
-for line in day2_content:
+for line in day2_input:
     if line[0] == "f":
         ch21_count_horizontal += int((line.split(' ')[1]))
     if line[0] == "d":
@@ -51,7 +50,7 @@ for line in day2_content:
     if line[0] == "u":
         ch21_count_depth -= int((line.split(' ')[1]))
 
-# print("Challenge 2.1 answer:", ch21_count_horizontal * ch21_count_depth)
+print("Challenge 2.1 answer:", ch21_count_horizontal * ch21_count_depth)
 
 # Challenge 2.2
         
@@ -59,7 +58,7 @@ ch22_count_horizontal = 0
 ch22_count_aim = 0
 ch22_count_depth = 0
 
-for line in day2_content:
+for line in day2_input:
     if line[0] == "f":
         ch22_count_horizontal += int((line.split(' ')[1]))
         ch22_count_depth += (ch22_count_aim * int((line.split(' ')[1])))
@@ -68,8 +67,31 @@ for line in day2_content:
     if line[0] == "u":
         ch22_count_aim -= int((line.split(' ')[1]))
 
-# print("Challenge 2.2 answer:", ch22_count_horizontal * ch22_count_depth)
+print("Challenge 2.2 answer:", ch22_count_horizontal * ch22_count_depth)
         
 # DAY 3
 
 # Challenge 3.1
+        
+# Since the task is basically to find out whether 1's or 0' dominate in each column,
+# I count the sum of each column (which will also be the sum of 1's) and check if the 1's
+# make up more than half of the column, and assign values accordingly.
+        
+gamma = ""
+epsilon = ""
+input3_length = len(day3_input[0])
+counter = 0
+
+for i in range(input3_length):
+
+    for elm in day3_input:
+        counter += int(elm[i])
+    if counter > (len(day3_input) / 2):
+        gamma += "1"
+        epsilon += "0"
+    else:
+        gamma += "0"
+        epsilon += "1"
+    counter = 0
+
+print("Challenge 3.1 answer:", int(gamma, 2) * int(epsilon, 2))
